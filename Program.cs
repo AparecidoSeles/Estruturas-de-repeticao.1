fa00a099d2fb5a393bc97d0623e59d8d0726f2a6
junior <you@example.com>
2020-12-28 17:58:10 -0300

    estruturas de repetição exerc 1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace atividade._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+
+            
+
+            Console.WriteLine("Digite sua nota de 0 a 10");
+            int resposta = int.Parse(Console.ReadLine());
+            
+        //    if (resposta >0 && resposta <=10)
+        //    {}
+          while(resposta <0 || resposta >10)
+          {
+                Console.WriteLine("Digite sua nota novamente");
+                resposta = int.Parse(Console.ReadLine());
+           }
+        }
+    }
+}
